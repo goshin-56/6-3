82629c2003ec09a374ecfe68ec6473e70b1ee479
goshin-56 <102846714+goshin->
2022-05-02 06:13:26 +0000

    [Add]5-3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.comments.new(comment_params)
-    comment.book_id = post.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    redirect_to book_path(@book)
   end
 
   def destroy
-    Comment.find_by(id: params[:id], book_id: params[:post_id]).destroy
+    Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     redirect_to book_path(params[:book_id])
   end
 
