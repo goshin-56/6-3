626496461702c3562945bbde7fcf8bc415f1c9af
goshin-56 <102846714+goshin->
2022-05-05 09:07:42 +0000

    [Add]6-1

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,14 +4,14 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     current_user.follow(user)
-    redirect_to root_path
+    redirect_to users_path
   end
 
   # フォロー外すとき
   def destroy
     user = User.find(params[:user_id])
     current_user.unfollow(user)
-    redirect_to root_path
+    redirect_to users_path
   end
 
 end
